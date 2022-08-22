0d21ea19c50e02f334c554a69a3cdca22fe2b0ec
Patrizio Fanelli <p.fanelli@joiasoftware.com>
2022-08-22 13:30:39 +0200

    fixi vm exception

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def read_template(filename):
 
+def read_template(filename):
     """
     Returns a Template object comprising the contents of the 
     file specified by filename.
@@ -19,7 +19,7 @@ def read_template(filename):
         template_file_content = template_file.read()
     return Template(template_file_content)
 
-def inviaMail():
+def inviaMail(errori):
     s = smtplib.SMTP(
         host= appcfg.get('sendMail','MAIL_HOST',fallback = 'localhost'),
         port= appcfg.get('sendMail','MAIL_PORT',fallback = 25))
@@ -31,18 +31,21 @@ def inviaMail():
         )
 
     msg = MIMEMultipart()       # create a message
-
+    message= "\n"
     # add in the actual person name to the message template
-    message = message_template_header.substitute(PERSON_NAME='TEST')
-
-    # Prints out the message body for our sake
-    print(message)
+    for e in errori:
+        if (e['type'] == 'VM'):
+            message += message_template_vm.substitute(**e)
+            message += "\n"
+        else :
+            mapping ={'address':e['Address'], 'name':e['Internal Name'], 'status':e['Status']} 
+            message += message_template_server.substitute(**mapping)
+            message += "\n"
 
     # setup the parameters of the message
     msg['From'] = appcfg.get('sendMail','MAIL_FROM')
     msg['To'] = appcfg.get('sendMail','MAIL_TO')
-    msg['Subject'] = "Riepilogo CLOUD TEST"
-
+    msg['Subject'] = "Errori VM CLOUD "
     # add in the message body
     msg.attach(MIMEText(message, 'plain'))
 
@@ -64,11 +67,11 @@ def inviaMail():
     pass
 
 def getHost(host,cfg):
+    vmtoskip = cfg.get('DEFAULT','vm-to-skip',fallback=[])
     r = {}
     r['Address'] = host
     r['Internal Name'] = cfg.get(host,'INTERNAL_NAME', fallback='non presente')
     skip = cfg.get(host,'SKIP', fallback='0')
-    print(skip,host)
     if(skip == '1'):
         r['SKIP'] = 'Controllo disabilitato'
         return r
@@ -83,11 +86,14 @@ def getHost(host,cfg):
     except ResourceException as err:
         print(err)
         r['Status'] = 'ERRORE'
+        r['type'] = 'SERVER'
         return r
     try:
         nodes = proxmox.nodes.get()
     except:
         r['Status'] = 'ERRORE Host non Connesso'
+        r['type'] = 'SERVER'
+        errori.append(r)
         return r
 
     for node in nodes:
@@ -96,15 +102,15 @@ def getHost(host,cfg):
         r['Status'] = n["status"]
         r['VMs'] = []
         for vm in proxmox.nodes(n["node"]).qemu.get():
-            #print ("{0}. {1} => {2}".format(vm["vmid"], vm["name"], vm["status"]))
             r['VMs'].append(dict({'VM id':vm["vmid"], 'VM name': vm["name"],'VM stato': vm["status"]}))
+            if(vm["status"] != 'running' and str(vm["vmid"]) not in vmtoskip):
+                errori.append(dict({'id':vm["vmid"], 'name': vm["name"],'stato': vm["status"],'cloud':r['Internal Name'],'type':'VM'}))
     return r
 
 def main(config_file):
     l = []
     file = Path(Path.cwd() / CONFIG_DIR / PRODUCTION_FILE)
     cfg = getConfig(config_file= config_file)
-    
     for host in cfg.sections():
         l.append(getHost(host,cfg))
     with open(file, 'w') as f:
@@ -140,10 +146,9 @@ def getConfig(config_file = 'config.ini'):
     return cfg
 pass
 
-#TODO backup
-#TODO LOG e ERR
 
 if __name__ == "__main__":
+    errori = []
     appcfg = appConfig()
     CONFIG_DIR = appcfg.get('DEFAULT','CONFIG_DIR',fallback='personal_data')
     ERROR_DIR = appcfg.get('DEFAULT','ERROR_DIR',fallback='personal_data/err')
@@ -152,8 +157,8 @@ if __name__ == "__main__":
     PROXMON_INI = appcfg.get('DEFAULT','PROXMOX_LIST',fallback='config.ini')
     PRODUCTION_FILE = appcfg.get('DEFAULT','PRODUCTION_FILE',fallback='list-server.json')
     dict_list = main(PROXMON_INI)
-    message_template_header = read_template(Path(Path.cwd() / CONFIG_DIR / 'header.txt'))
-    message_template_body = read_template(Path(Path.cwd() / CONFIG_DIR / 'body.txt'))
-    
-    if int(appcfg.get('sendMail','ENABLE',fallback = 0)):
-        inviaMail()
+    message_template_vm = read_template(Path(Path.cwd() / CONFIG_DIR / 'txterror-vm.txt'))
+    message_template_server = read_template(Path(Path.cwd() / CONFIG_DIR / 'txterror-server.txt'))
+
+    if int(appcfg.get('sendMail','ENABLE',fallback = 0) and len(errori)):
+        inviaMail(errori)
